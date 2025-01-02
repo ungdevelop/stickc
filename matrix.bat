a3c5488bc8c770c49d1fcf9215b0a5d8da8190a9
ungdevelop <romashka.kisnsvr@gmail.com>
2025-01-02 16:24:59 +0500

    Add files via upload

---
+++ matrix.bat
@@ -0,0 +1,5 @@
+@echo off
+color 0a
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
